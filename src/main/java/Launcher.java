8a2717ff4450a7884aa0130e40e271f51672c3de
Alexander Naryadchikov <alexnar1771@gmail.com>
2017-05-20 20:54:25 +0300

    Добавлена тестовая сеть для работы с музыкой.

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,71 +1,82 @@
+import music.AwesomeMusic;
+
 /**
  * Класс-Launcher.
  */
-import music.AwesomeMusic;
-import music.WaveFile;
-
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.File;
-import java.io.IOException;
-
-import static music.WaveFile.*;
-
 public class Launcher {
-    int bitrate = 44100;
-
-    public  float[] FORSASHENIAKA(String FileName){
-        int DLINAPESNI = 90000000; // bitrate * t , где t - длина песни в секундах
-        float [] tmp = new float[DLINAPESNI];
-
-        System.out.println("Чтение данных из моно-файла:");
-        WaveFile wf = null;
-        try {
-            wf = new WaveFile(new File(FileName)); //"./src/main/resources/testwav1.wav"
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        for (int i = 0; i < DLINAPESNI; i++) {
-           tmp[i] =  wf.getSampleInt(i);
-        }
-        return tmp;
-    }
 
     public static void main(String[] args) {
-        createWave();
+//        NeuralNetwork XoRNetwork = new NeuralNetwork(0.7f, 0.3f);
+//        float[][] inputData = {{0.0f, 0.0f}, {0.0f, 1.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}};
+//        float[][] idealData = {{0.0f}, {1.0f}, {1.0f}, {0.0f}};
+//        boolean isWithBias = true;
+//        int lastEpoch = 100000;
+//
+//        XoRNetwork.addLayer(new InputLayer(isWithBias, inputData[0]));
+//        XoRNetwork.addLayer(new HiddenLayer(isWithBias, 3));
+//        XoRNetwork.addLayer(new OutputLayer(idealData[0]));
+//
+//        XoRNetwork.start();
+//
+//        for (int i = 0; i < lastEpoch; i++) {
+//            for (int j = 0; j < idealData.length; j++) {
+//                XoRNetwork.changeInputData(isWithBias, inputData[j]);
+//                XoRNetwork.changeIdealData(idealData[j]);
+//                XoRNetwork.train();
+//            }
+//        }
+//
+//        float[][] testData = {{0.0f, 0.0f}, {1.0f, 0.0f}};
+//        float[][] idealTestResult = {{0.0f}, {1.0f}};
+//        float[] resultData;
+//
+//        for (int i = 0; i < testData.length; i++) {
+//            XoRNetwork.changeInputData(isWithBias, testData[i]);
+//            XoRNetwork.changeIdealData(idealTestResult[i]);
+//
+//            resultData = XoRNetwork.getResultData();
+//
+//            System.out.println("Final result is: " + resultData[0]);
+//            System.out.println("Ideal answer is: " + idealTestResult[i][0]);
+//        }
+        AwesomeMusic[] trainingSongs = new AwesomeMusic[10];
+        float[][] inputData = { {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+            {0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+            {1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+            {1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f} };
 
-        NeuralNetwork XoRNetwork = new NeuralNetwork(0.7f, 0.3f);
-        float[][] inputData = {{0.0f, 0.0f}, {0.0f, 1.0f}, {1.0f, 0.0f}, {1.0f, 1.0f}};
-        float[][] idealData = {{0.0f}, {1.0f}, {1.0f}, {0.0f}};
-        boolean isWithBias = true;
-        int lastEpoch = 100000;
-
-        XoRNetwork.addLayer(new InputLayer(isWithBias, inputData[0]));
-        XoRNetwork.addLayer(new HiddenLayer(isWithBias, 3));
-        XoRNetwork.addLayer(new OutputLayer(idealData[0]));
-
-        XoRNetwork.start();
-
-        for (int i = 0; i < lastEpoch; i++) {
-            for (int j = 0; j < idealData.length; j++) {
-                XoRNetwork.changeInputData(isWithBias, inputData[j]);
-                XoRNetwork.changeIdealData(idealData[j]);
-                XoRNetwork.train();
-            }
+        for (int i = 0; i < trainingSongs.length; i++) {
+            trainingSongs[i] = new AwesomeMusic("InsertPathToFileHere");
         }
 
-        float[][] testData = {{0.0f, 0.0f}, {1.0f, 0.0f}};
-        float[][] idealTestResult = {{0.0f}, {1.0f}};
-        float[] resultData;
+        NeuralNetwork[] YetiSoundNetworks = new NeuralNetwork[trainingSongs[0].getFramesCount()];
+        boolean isWithBias = true;
 
-        for (int i = 0; i < testData.length; i++) {
-            XoRNetwork.changeInputData(isWithBias, testData[i]);
-            XoRNetwork.changeIdealData(idealTestResult[i]);
+        for (int i = 0; i < YetiSoundNetworks.length; i++) {
+            YetiSoundNetworks[i] = new NeuralNetwork(0.005f, 0.01f);
+        }
 
-            resultData = XoRNetwork.getResultData();
+        for (int i = 0; i < YetiSoundNetworks.length; i++) {
+            float[][] idealData = new float[trainingSongs.length][1];
+            int lastEpoch = 10000;
 
-            System.out.println("Final result is: " + resultData[0]);
-            System.out.println("Ideal answer is: " + idealTestResult[i][0]);
+            for (int j = 0; j < trainingSongs.length; j++) {
+                idealData[j][0] = trainingSongs[j].getIdealData()[i];
+            }
+
+            YetiSoundNetworks[i].addLayer(new InputLayer(isWithBias, inputData[0]));
+            YetiSoundNetworks[i].addLayer(new HiddenLayer(isWithBias, 3));
+            YetiSoundNetworks[i].addLayer(new OutputLayer(idealData[0]));
+
+            YetiSoundNetworks[i].start();
+
+            for (int j = 0; j < lastEpoch; j++) {
+                for (int k = 0; k < idealData.length; k++) {
+                    YetiSoundNetworks[i].changeInputData(isWithBias, inputData[k]);
+                    YetiSoundNetworks[i].changeIdealData(idealData[k]);
+                    YetiSoundNetworks[i].train();
+                }
+            }
         }
     }
 }
--- src/main/java/music/AwesomeMusic.java
+++ src/main/java/music/AwesomeMusic.java
@@ -1,24 +1,45 @@
 package music;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.File;
 
+/**
+ * Класс, содержащий идеальные данные для тренировки сети.
+ */
 public class AwesomeMusic {
 
-    private ArrayList<Float> frames;
+    private WaveFile waveFile;
+    private int framesCount;
 
-    public AwesomeMusic(float[] frames) {
-        this.frames = new ArrayList<Float>();
-        for (Float frame : frames) {
-            this.frames.add(frame);
+    public AwesomeMusic(String FileName) {
+        try {
+            waveFile = new WaveFile(new File(FileName));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        framesCount = new Long(waveFile.getFramesCount()).intValue();
     }
 
-    public int getAmountOfFrames() {
-        return frames.size();
+    /**
+     * Возвращает массив идеальных данных для тренировки сети.
+     * @return Массив идеальных данных.
+     */
+    public float[] getIdealData() {
+        float[] idealData = new float[framesCount];
+
+        if (waveFile != null) {
+            for (int i = 0; i < framesCount; i++) {
+                idealData[i] = waveFile.getSampleInt(i);
+            }
+        }
+
+        return idealData;
     }
 
-    public float getIdealData(int index) {
-        return frames.get(index);
+    /**
+     * Возвращает количество кадров в песне
+     * @return Количество кадров в песне.
+     */
+    public int getFramesCount() {
+        return framesCount;
     }
 }
